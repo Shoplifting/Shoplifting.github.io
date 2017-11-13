4bd3cc5f387d7742321679fb9cf9ae3449866ce3
David Hew-wing <dheww012@uottawa.ca>
2017-11-12 19:31:19 -0500

    JS Submit fadeout finished

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function messageRecieved(){
+	console.log("test")
 	$("#submitButton").fadeOut();
 	var para = $("<p>Your message has been received!</p>");
 	$("#submitDiv").append(para);
+	return false;
 }	
